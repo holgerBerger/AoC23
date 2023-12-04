7de5c219e9c5aaa9d2852b0d6c923a919de94a35
Holger Berger <holger.berger@googlemail.com>
2023-12-04 19:26:49 +0100

    solution part1

---
+++ Day4/part1.d
@@ -0,0 +1,40 @@
+import std.stdio;
+import std.ascii;
+import std.algorithm;
+
+int getpoints(char[] line) {
+    int pos=0;
+    char lc,c;
+    int value, number;
+    bool[int] numbers;
+
+    line ~= " |";
+
+    while(line[pos]!=':') pos++; pos++;
+    
+    while((c=line[++pos])!='|') {
+        if (isDigit(c)) number=number*10+c-'0';
+        else {
+            if(number>0) numbers[number]=true;
+            number=0;
+        }    
+    }
+
+    while((c=line[++pos])!='|') {
+        if (isDigit(c)) number=number*10+c-'0';
+        else {
+            if(number>0) {
+                if (number in numbers) 
+                    (value==0) ? (value=1) : (value*=2); 
+                number=0;
+            }
+        }    
+    }
+    
+    return value;
+}
+
+void main() {
+    stdout.writeln(stdin.byLine.map!getpoints.sum);
+}
+
