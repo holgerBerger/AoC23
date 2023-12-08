6a3e584f4eb1848e466875ccd1a483d032b99d47
Holger Berger <holger.berger@googlemail.com>
2023-12-08 10:14:43 +0100

    day7 part2

---
+++ Day7/part2.d
@@ -0,0 +1,67 @@
+// D solition Day7 part2
+
+import std.stdio;
+import std.algorithm;
+import std.array;
+import std.conv;
+
+short[dchar] cardmap = [ 'A': 14, 'K': 13, 'Q':12, 'T':10, '9': 9, '8':8, '7':7, '6':6, '5':5, '4':4, '3':3, '2':2, 'J':1 ];
+
+alias Hand = short[5];
+
+struct Handsbid {
+    Hand hand;
+    Handtype type;
+    int bid;
+}
+
+enum Handtype { FIVE=8, FOUR=7, FULL=6, THREE=5, TWOPAIR=4, ONEPAIR=3, HIGH=2, NONE=1};
+
+Handtype gettype(Hand hand) {
+    short[short] counter;
+    short jokers;
+    foreach(c; hand) {
+        if (c == 1) jokers++;
+        else
+            if (c in counter) counter[c]++; else counter[c]=1;
+    }
+    debug stdout.write(counter, " J=", jokers);
+    auto v =  counter.byValue.array.sort.reverse.array;
+    if (jokers == 5) return Handtype.FIVE;
+    v[0]+=jokers;
+    if (v[0] == 5) return Handtype.FIVE;
+    if (v[0] == 4) return Handtype.FOUR;
+    if (v[0] == 3 && v[1] == 2) return Handtype.FULL;
+    if (v[0] == 3) return Handtype.THREE;
+    if (v[0] == 2 && v[1] == 2) return Handtype.TWOPAIR;
+    if (v[0] == 2) return Handtype.ONEPAIR;
+    if (v == [1,1,1,1,1]) return Handtype.HIGH;
+    return Handtype.NONE;
+}
+
+Handsbid parse(const char[] line) {
+    auto sp = line.split();
+    short[5] hand = sp[0].map!(a=>cardmap[a]).array;
+    auto t = gettype(hand);
+    debug stdout.writeln(" -> ", t);
+    return Handsbid(hand, t, to!int(sp[1]));
+    //return Handsbid(hand, gettype(hand), to!int(sp[1]));
+}
+
+pure bool handcomp(in Handsbid a, in Handsbid b) {
+    if (a.type < b.type) return true;
+    if (a.type > b.type) return false;
+    for(int i=0; i<5; i++) {
+        if (a.hand[i] < b.hand[i]) return true;
+        if (a.hand[i] > b.hand[i]) return false;
+    }
+    assert(0);
+}
+
+void main() {
+    auto handsbid = stdin.byLine.map!parse.array.sort!(handcomp);
+    long winning;
+    //stdout.writeln(handsbid);
+    for(int i=0; i<handsbid.length; i++) winning+=(i+1)*handsbid[i].bid;
+    stdout.writeln(winning);
+}
